aa76e5e8dfa42ea58480da4a196fbbfadcd3696f
Huydzsss <144263815+Huydzsss@users.noreply.github.com>
2025-05-02 16:29:58 +0700

    up api úp ảnh

--- src/main/java/org/aptech/backendmypham/controllers/CustomerDetailController.java
+++ src/main/java/org/aptech/backendmypham/controllers/CustomerDetailController.java
@@ -13,18 +13,18 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/api/v1/customer/")
 @RequiredArgsConstructor
 public class CustomerDetailController {
-    private final CustomerDetailService customerService;
+    private final CustomerDetailService customerDetailService;
 
     @PostMapping("/login")
     @Operation(summary = "đăng nhập cho khách hàng")
     public ResponseEntity<ResponseObject> loginCustomer(@RequestBody LoginCustomerDto Cdto) {
-        return ResponseEntity.ok(customerService.loginCustomer(Cdto));
+        return ResponseEntity.ok(customerDetailService.loginCustomer(Cdto));
     }
     @PostMapping("/register")
     @Operation(summary = "đăng ký cho khách hàng")
     public ResponseEntity<ResponseObject> registerCustomer(@RequestBody RegisterRequestDto registerRequestDto) {
         try {
-            ResponseObject user = customerService.registerCustomer(registerRequestDto);
+            ResponseObject user = customerDetailService.registerCustomer(registerRequestDto);
             return ResponseEntity.ok(new ResponseObject(Status.SUCCESS, "Đăng ký thành công", user));
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(new ResponseObject(Status.ERROR, e.getMessage(), null));
@@ -33,7 +33,7 @@ public class CustomerDetailController {
     @GetMapping("/detail/{id}")
     @Operation(summary = "Thông tin chi tiết về khách hàng")
     public ResponseEntity<ResponseObject> detail(@PathVariable Long id) {
-        return ResponseEntity.ok(customerService.getCustomerDetail(id));
+        return ResponseEntity.ok(customerDetailService.getCustomerDetail(id));
     }
 
     @PutMapping(value = "/update-info/{id}", consumes = {"multipart/form-data"})
@@ -42,19 +42,19 @@ public class CustomerDetailController {
             @PathVariable Long id,
             @RequestPart("info") CustomerDetailResponseDto Cdto,
             @RequestPart(value = "file", required = false) MultipartFile file) {
-        return ResponseEntity.ok(customerService.updateCustomer(id, Cdto, file));
+        return ResponseEntity.ok(customerDetailService.updateCustomer(id, Cdto, file));
     }
 
 
     @PutMapping("/change-password/{id}")
     public ResponseEntity<ResponseObject> changePassword(@PathVariable Long id, @RequestBody ChangePasswordCustomerRequestDto dto) {
-        return ResponseEntity.ok(customerService.changePasswordCustomer(dto,id));
+        return ResponseEntity.ok(customerDetailService.changePasswordCustomer(dto,id));
     }
 
 
 
     @PostMapping("/logout")
     public ResponseEntity<ResponseObject> logout() {
-        return ResponseEntity.ok(customerService.logout());
+        return ResponseEntity.ok(customerDetailService.logout());
     }
 }
