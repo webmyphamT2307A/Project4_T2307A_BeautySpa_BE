a90be8da1f966ab686fed4bb73593a0a9e240431
Huydzsss <144263815+Huydzsss@users.noreply.github.com>
2025-05-20 20:10:43 +0700

    update thêm về BE điểm danh

---
+++ src/main/java/org/aptech/backendmypham/services/AttendanceService.java
@@ -0,0 +1,9 @@
+package org.aptech.backendmypham.services;
+
+import org.aptech.backendmypham.models.Attendance;
+
+import java.util.List;
+
+public interface AttendanceService {
+    public List<Attendance> getAll();
+}
