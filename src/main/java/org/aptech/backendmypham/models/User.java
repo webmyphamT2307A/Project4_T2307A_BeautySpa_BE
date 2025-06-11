b947be6a5005ae80cdf3863d0ba6656a91912c65
Huydzsss <144263815+Huydzsss@users.noreply.github.com>
2025-06-11 13:59:49 +0700

    Update User.java

--- src/main/java/org/aptech/backendmypham/models/User.java
+++ src/main/java/org/aptech/backendmypham/models/User.java
@@ -22,7 +22,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id", nullable = false)
-    private Integer id;
+    private Long id;
 
     @Column(name = "full_name", nullable = false)
     private String fullName;
