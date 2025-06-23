602bf38d2af18cfe08755f8c03708a1476a48909
Huydzsss <144263815+Huydzsss@users.noreply.github.com>
2025-06-23 22:43:59 +0700

    Update AppointmentService.java

--- src/main/java/org/aptech/backendmypham/services/AppointmentService.java
+++ src/main/java/org/aptech/backendmypham/services/AppointmentService.java
@@ -26,8 +26,5 @@ public interface AppointmentService {
 
     Map<String, Object> getAppointmentsGroupedByShift(LocalDate date, Long userId);
     List<AppointmentResponseDto> getAppointmentsByUserId(Long userId);
-    List<AppointmentHistoryDTO> getCustomerAppointmentHistory(Long customerId, int page, int size);
-    List<AppointmentHistoryDTO> getAppointmentHistoryByPhone(String phoneNumber);
-    AppointmentStatsDTO getCustomerAppointmentStats(Long customerId);
-
+  
 }
